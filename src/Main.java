0c5e2c140726e3cb59187f44c20f4faab29d5d5f
Raed <raed.fawzy@gmail.com>
2020-11-10 14:37:49 -0500

    Initial Commit1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         // b21 is good at /Intellij
+        //I have already publish on /github.com
+        //then commit without sharing
     }
 }
